776f76154d63550b927ea1901d88797b40d36331
Erick Mayorga Juarez <erickmayorga7562@gmail.com>
2025-12-12 19:14:49 -0600

    Ultimos Cambios realizados

--- Portfolio/Models/SocialLinks.cs
+++ Portfolio/Models/SocialLinks.cs
@@ -1,12 +1,14 @@
 ﻿using System.EnterpriseServices.Internal;
-
-public class SocialLinks
+namespace Portfolio.Models
 {
-    public int Id { get; set; }
-    public string Platform { get; set; } //Red social  (Linkedin, GitHub)
-    public string Url { get; set; }
+    public class SocialLinks
+    {
+        public int Id { get; set; }
+        public string Platform { get; set; } //Red social  (Linkedin, GitHub)
+        public string Url { get; set; }
 
-    public int StudentProfileId {get; set;}
+        public int StudentProfileId { get; set; }
 
-    public StudentProfile StudentProfile {get; set;}
+        public StudentProfile StudentProfile { get; set; }
+    }
 }
